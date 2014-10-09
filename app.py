f0e438224cb2f3b7f278ccc258d1ef2d0b5c064b
Evan Dorsky <evan.dorsky@icloud.com>
2014-10-09 13:48:06 -0400

    Clean up backend a bit

--- app.py
+++ app.py
@@ -4,6 +4,21 @@ import thinkbayes2 as tb
 app = Flask(__name__)
 print(__name__)
 
+"""
+I want a way to package up pmfs and send them.
+Right now it's an object:
+
+{"pmf": [[outcome, prob], [outcome, prob], ...]}
+
+This works fine.
+"""
+
+def packaged(pmf):
+	return json.jsonify({ "pmf": zip(pmf.d.keys(), pmf.d.values()) })
+
+def suiteupdate(request):
+	return dict(request['pmf']), request['update']
+
 @app.route("/")
 def hello():
 	return render_template("index.html")
@@ -15,9 +30,8 @@ def pmf():
 		for i in range(1, 101):
 			pmf.Set(i, 1)
 		pmf.Normalize()
-		suitejson = json.jsonify({ "pmf": zip(pmf.d.keys(), pmf.d.values()) })
 
-		return suitejson
+		return packaged(pmf)
 	else:
 		class Euro(tb.Suite):
 			def Likelihood(self, data, hypo):
@@ -27,37 +41,12 @@ def pmf():
 				else:
 					return 1-x
 
-		pmf = dict(request.get_json()['pmf'])
-		update = request.get_json()['update']
+		pmf, update = suiteupdate(request.get_json())
 
 		euro = Euro(tb.MakePmfFromDict(pmf))
 		euro.Update(update)
 
 		return json.jsonify({ "pmf": zip(euro.d.keys(), euro.d.values()) })
-
-"""
-posting here with a pmf and an update
-responds with the updated version of the pmf
-"""
-
-"""
-a user interaction
-enter data
-	create a named pmf (server)
-make an interactive graph
-	graph the pmf data (client)
-interact with the graph, see live updates
-	requests for new data (server)
-	update graph (client)
-"""
-
-"""
-hold state on client or server?
-definitely client... javascript objects hold data
-server just fulfills request for data, operations
-
-PMF: javascript object {outcome: probability}
-"""
 
 if __name__ == "__main__":
 	app.run(debug=True)
